861fbd7bfd3e0dc884e8b5bf881a83816e62c75b
Ian Torres <iantorres@outlook.com>
2024-07-15 21:51:57 -0400

    Covered.

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $user = User::query()->create([
+            "name" => "John Doe",
+            "email" => "john@doe.com",
+            "password" => "password",
+        ]);
+
+        $this->assertEquals("john@doe.com", $user->email);
+    }
+}
